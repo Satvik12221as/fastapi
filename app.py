b357c997871c37b8308e2f96620ab6b4c4f93b11
Satvik12221as <sanu8800971634@gmail.com>
2025-10-09 10:48:46 +0530

    post

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -23,3 +24,12 @@ def about(id : int):
 @app.get("/blog/{id}/comments")
 def comments(id):
     return {"data" : {'1', '2'}}
+
+class Blog(BaseModel):
+    title : str
+    body : str
+    published : Optional[bool]
+
+@app.post('/blog')
+def create_blog(blog : Blog):
+    return {"data" : f'blog is created as {blog.title}'}
