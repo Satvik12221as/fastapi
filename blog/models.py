4783d54cec12c674c7704300cbdb6d6d32e03a77
Satvik12221as <sanu8800971634@gmail.com>
2025-11-12 16:54:04 +0530

    blog connected to database

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
 from .database import Base
 from sqlalchemy import Column, Integer, String, Boolean
 
-
+# Create Blog model
 class Blog(Base):
 
+# table name and columns
     __tablename__ = 'blogs'
     id = Column(Integer , primary_key=True , index=True)
     title = Column(String)
