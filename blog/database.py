51e9356cdcbbf012430ea13a61be25cd9f9d4e97
Satvik12221as <sanu8800971634@gmail.com>
2025-11-12 16:54:49 +0530

    blog connected to database

--- blog/database.py
+++ blog/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 # Database URL
 SQLALCHAMY_DATABASE_URL = 'sqlite:///./blog.db'
 
-
+# Create engine
 engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
