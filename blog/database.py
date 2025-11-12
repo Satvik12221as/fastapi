c0f5a5ad7418ed43bda4457e5e053aa82a1f6549
Satvik12221as <sanu8800971634@gmail.com>
2025-11-12 16:47:00 +0530

    blog connected to database

--- blog/database.py
+++ blog/database.py
@@ -8,6 +8,6 @@ SQLALCHAMY_DATABASE_URL = 'sqlite:///./blog.db'
 
 engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
-sessionmaker = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- blog/main.py
+++ blog/main.py
@@ -1,13 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Depends , status , Response , HTTPException
 from . import schemas
 from . import models
-from .database import engine
-
+from .database import engine , SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine) 
 
-@app.post('/blog')
-def create(request : schemas.Blog):
-    return {'request'}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post('/blog' , status_code = 201)
+def create(request : schemas.Blog , db : Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title , body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+@app.get('/blog')
+def all(db:Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+
+@app.get('/blog/{id}' , status_code=200)
+def show(id , response : Response, db : Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'blog with the id {id} is not available')
+        #response.status_code = status.HTTP_404_NOT_FOUND
+        #return {'detail' : f'blog with this id {id} is not available'}
+    return blog
