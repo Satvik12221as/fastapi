457b7d608a0d24d57290bff53e435024ef34de1d
Satvik12221as <sanu8800971634@gmail.com>
2025-11-13 18:39:44 +0530

    updation of blogs

--- blog/main.py
+++ blog/main.py
@@ -43,3 +43,19 @@ def show(id , response : Response, db : Session = Depends(get_db)):
         #response.status_code = status.HTTP_404_NOT_FOUND
         #return {'detail' : f'blog with this id {id} is not available'}
     return blog
+
+
+
+# Delete blog
+@app.delete('/blog/{id}' , status_code=status.HTTP_204_NO_CONTENT)
+def delete(id , db : Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+
+
+@app.put('/blog/{id}' , status_code=status.HTTP_202_ACCEPTED)
+def update(id , request : schemas.Blog ,  db : Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).update({'title' : request.title , 'body' : request.body})
+    db.commit()
+    return 'updated'
