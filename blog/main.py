e90be0d369b3195efbb2fe377342fc24a4700978
Satvik12221as <sanu8800971634@gmail.com>
2025-11-13 18:44:21 +0530

    updation of blogs

--- blog/main.py
+++ blog/main.py
@@ -49,13 +49,21 @@ def show(id , response : Response, db : Session = Depends(get_db)):
 # Delete blog
 @app.delete('/blog/{id}' , status_code=status.HTTP_204_NO_CONTENT)
 def delete(id , db : Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'blog with the id {id} is not available')
+    blog.delete(synchronize_session=False)
     db.commit()
     return 'done'
 
 
+
+# Update blog
 @app.put('/blog/{id}' , status_code=status.HTTP_202_ACCEPTED)
 def update(id , request : schemas.Blog ,  db : Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).update({'title' : request.title , 'body' : request.body})
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'blog with the id {id} is not available')
+    blog.update({'title' : request.title , 'body' : request.body})
     db.commit()
     return 'updated'
