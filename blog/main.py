edf01440b9c0ec1f8767576db11e69d7f08971ec
Satvik12221as <sanu8800971634@gmail.com>
2025-11-14 02:46:54 +0530

    showing without ID

--- blog/main.py
+++ blog/main.py
@@ -35,7 +35,7 @@ def all(db:Session = Depends(get_db)):
 
 
 # Get blog by id
-@app.get('/blog/{id}' , status_code=200)
+@app.get('/blog/{id}' , status_code=200 , response_model=schemas.ShowBlog)
 def show(id , response : Response, db : Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
